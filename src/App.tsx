a09bb4acc567cd7177c01ad7c0f597b94a1ccf06
George Andersen <g.andersen@hotmail.co.uk>
2021-09-22 08:51:04 +0100

    Tidy up code

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,27 @@
 import "./App.css";
-import { Chapter } from "./map/Chapter";
-import james from "./data/james.json";
+import { Chapter, GroupObj } from "./map/Chapter";
+import jamesData from "./data/james.json";
 
+const james = jamesData as GroupObj[][];
 function App() {
   return (
     <div className="App" style={{ height: "100vh" }}>
       <div className="board">
         <svg width="100%" height="100%">
           <g transform={`translate(50, 50)`}>
-            <Chapter length={27} groups={james[0] as any} />
+            <Chapter length={27} groups={james[0]} />
           </g>
           <g transform={`translate(400, 50)`}>
-            <Chapter length={26} groups={james[1] as any} />
+            <Chapter length={26} groups={james[1]} />
           </g>
           <g transform={`translate(50, 350)`}>
-            <Chapter length={18} groups={james[2] as any} />
+            <Chapter length={18} groups={james[2]} />
           </g>
           <g transform={`translate(275, 350)`}>
-            <Chapter length={17} groups={james[3] as any} />
+            <Chapter length={17} groups={james[3]} />
           </g>
           <g transform={`translate(500, 350)`}>
-            <Chapter length={20} groups={james[4] as any} />
+            <Chapter length={20} groups={james[4]} />
           </g>
         </svg>
       </div>
